ce420e475dd8eb14f278641260ad72c043f81074
meditrack819 <meditrack819@gmail.com>
2025-10-04 19:42:54 +0800

    Update Register.js

--- admin-meditrack/src/pages/Register.js
+++ admin-meditrack/src/pages/Register.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 
 // Hardcode backend API URL
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = process.env.REACT_APP_BACKEND_URL || "http://meditrack.space/api";
 
 export default function Register() {
   const [form, setForm] = useState({
@@ -132,3 +132,4 @@ export default function Register() {
     </div>
   );
 }
+
