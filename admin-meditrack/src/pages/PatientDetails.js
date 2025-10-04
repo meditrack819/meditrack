612f711b91a0892b736beac306663ec19e253097
meditrack819 <meditrack819@gmail.com>
2025-10-04 19:43:44 +0800

    Update PatientDetails.js

--- admin-meditrack/src/pages/PatientDetails.js
+++ admin-meditrack/src/pages/PatientDetails.js
@@ -6,8 +6,8 @@ import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend
 } from "recharts";
 
-const API_BASE = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
-const API = `${API_BASE}/api/patients`;
+const API_BASE = process.env.REACT_APP_BACKEND_URL || "http://meditrack.space/api";
+const API = `${API_BASE}/patients`;
 
 /* ---------------- Labels (Tagalog, DOH Form) ---------------- */
 const FIELD_LABELS = {
@@ -467,4 +467,5 @@ export default function PatientDetails({ patientId, onClose }) {
     </>
   );
 }
+
 
