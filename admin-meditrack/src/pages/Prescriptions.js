756d7207238e097f41be139dd6f17fb3eb483da3
meditrack819 <meditrack819@gmail.com>
2025-10-05 19:27:15 +0800

    Update Prescriptions.js

--- admin-meditrack/src/pages/Prescriptions.js
+++ admin-meditrack/src/pages/Prescriptions.js
@@ -3,9 +3,9 @@ import React, { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 
-const API_BASE = process.env.REACT_APP_BACKEND_URL || "http://meditrack.space/api";
-const RX_API = `${API_BASE}/prescriptions`;
-const PT_API = `${API_BASE}/patients`;
+const API_BASE = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
+const RX_API = `${API_BASE}api/prescriptions`;
+const PT_API = `${API_BASE}/api/patients`;
 
 const Styles = () => (
   <style>{`
@@ -436,5 +436,6 @@ export default function Prescriptions() {
     </div>
   );
 }
+
 
 
