4dc86adf03dc48769343b0e0273e09b759f783f8
meditrack819 <meditrack819@gmail.com>
2025-10-05 19:28:02 +0800

    Update AdminLogin.js

--- admin-meditrack/src/pages/AdminLogin.js
+++ admin-meditrack/src/pages/AdminLogin.js
@@ -5,7 +5,7 @@ import { useNavigate, Link } from "react-router-dom";
 
 // ✅ Correct base URL
 const API_BASE = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
-const url = `${API_BASE}/api/auth/staff/login`;
+const url = `${API_BASE}/auth/staff/login`;
 
 export default function AdminLogin() {
   const [email, setEmail] = useState("");
@@ -93,4 +93,5 @@ export default function AdminLogin() {
     </div>
   );
 }
+
 
